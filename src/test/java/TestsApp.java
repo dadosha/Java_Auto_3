53ac942c0360c84c3be88aa38e36fa2926355f57
Станислав Койфман <stanislavkoyfman@gmail.com>
2024-05-05 22:31:34 +0300

    Edit gradle.yml

--- src/test/java/TestsApp.java
+++ src/test/java/TestsApp.java
@@ -3,7 +3,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
